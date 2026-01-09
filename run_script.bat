ef8d8f6d24b7f3fcfe90b2f2b4f8b8c25171a928
Anzala Ajra <dang.anzala@gmail.com>
2026-01-09 20:43:09 +0700

    track git

---
+++ run_script.bat
@@ -0,0 +1,5 @@
+@echo off
+timeout /t 5 /nobreak >nul
+cd /d "C:\Checkin"
+start "" /b "C:\Users\Dell 2 - 4HX3754\AppData\Local\Microsoft\WindowsApps\pythonw.exe" "C:\Checkin\film_televisi_checkin.py"
+exit
